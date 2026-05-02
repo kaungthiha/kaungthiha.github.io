3ddeb51712b6b344be8e781c5d8a27d7f41564ef
kaungthiha <65475312+kaungthiha@users.noreply.github.com>
2026-05-02 15:19:01 -0700

    Update tree.js

--- assets/js/tree.js
+++ assets/js/tree.js
@@ -185,7 +185,7 @@
 
   const canopies = [];
   TREE_LAYOUT.forEach(spec => {
-    const { group, canopy } = buildTree();
+    const { group, canopy } = buildTree(spec);
     group.position.set(spec.x, 0, spec.z);
     group.scale.setScalar(spec.s);
     group.rotation.y = (Math.random() - 0.5) * 0.6;
